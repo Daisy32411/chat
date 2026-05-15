7e95269ef421b7f79999b827ff790e57f99901d1
Kamil <kamilzaripov228@gmail.com>
2026-05-15 08:37:23 +0300

    modified taskfile.yaml 2.0

--- migrations/003_create_dialogs.up.sql
+++ migrations/003_create_dialogs.up.sql
@@ -1,11 +1,10 @@
 CREATE TABLE dialogs (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE dialog_members (
     dialog_id INT REFERENCES dialogs(id) ON DELETE CASCADE,
     username TEXT REFERENCES users(username) ON DELETE CASCADE,
-
     PRIMARY KEY (dialog_id, username)
 );
